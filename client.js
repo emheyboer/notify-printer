ec373322386a9969b3ff014305e7d49133cbce27
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-12-28 12:50:50 -0500

    login

---
+++ client.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+require('dotenv').config({quiet: true});
+
+const api_url = 'https://api.pushover.net/1';
+
+function check(response) {
+    if (response.status == 1) {
+        return;
+    }
+    response.errors.forEach(error => {
+        console.error(`error (status = ${response.status}): ${error}`);
+    })
+    console.log('shutting down...');
+    process.exit();
+}
+
+function retryFetch(resource, options, backoff = 500) {
+    function wait(delay){
+        return new Promise((resolve) => setTimeout(resolve, delay));
+    }
+    function onError(_err){
+        return wait(backoff).then(() => retryFetch(resource, options, backoff * 2));
+    }
+    return fetch(resource, options).catch(onError);
+}
+
+async function login(email, password) {
+    console.log('logging in...');
+
+    const parameters = {
+        email: email,
+        password: password,
+    };
+
+    const json = await retryFetch(api_url + '/users/login.json', {
+        method: 'post',
+        body: new URLSearchParams(parameters),
+    }).then(res => res.json());
+    check(json);
+
+    return json.secret;
+}
+
+async function main() {
+    const email = process.env['EMAIL'];
+    const password = process.env['PASSWORD'];
+    const secret = process.env['SECRET'] || await login(email, password);
+
+    console.log(`secret = ${secret}`);
+}
+
+main();
